ebb366d98110be37de3c664e63f41486ce880183
Sean Davis <sean@sdavismedia.com>
2015-06-07 11:49:20 -0500

    adjust settings to use defaults but allow blank values

--- includes/functions.php
+++ includes/functions.php
@@ -17,9 +17,9 @@ if( !defined( 'ABSPATH' ) ) exit;
  * @return      string The content displayed in the empty cart
  */
 function edd_empty_cart_content( $content ) {
-	$empty_cart_title          = edd_get_option( 'edd_empty_cart_title', __( 'Your cart is empty.', 'edd-empty-cart' ) );
-	$empty_cart_content        = edd_get_option( 'edd_empty_cart_content', __( 'It appears you do not have any items in your cart. Perhaps you would be interested in one of the items below.', 'edd-empty-cart' ) );
-	$empty_cart_downloads      = edd_get_option( 'edd_empty_cart_downloads_shortcode', '[downloads]' );
+	$empty_cart_title          = edd_get_option( 'edd_empty_cart_title' );
+	$empty_cart_content        = edd_get_option( 'edd_empty_cart_content' );
+	$empty_cart_downloads      = edd_get_option( 'edd_empty_cart_downloads_shortcode' );
 	
 	if ( !edd_is_checkout() ) {
 		$content = '<span>' . __( 'Your cart is empty.', 'edd-empty-cart' ) . '</span>';
@@ -28,21 +28,22 @@ function edd_empty_cart_content( $content ) {
 
 	ob_start();
 
-	if ( '' != $empty_cart_title ) {
+	if ( !isset( $empty_cart_title ) ) {
+		echo '<h3 class="edd-empty-cart-title">' . __( 'Your cart is empty.', 'edd-empty-cart' ) . '</h3>';
+	} else {
 		echo '<h3 class="edd-empty-cart-title">' . $empty_cart_title . '</h3>';
 	}
 
-	if ( '' != $empty_cart_content ) {
+	if ( !isset( $empty_cart_content ) ) {
+		echo '<div class="edd-empty-cart-content">' . wpautop( __( 'It appears you do not have any items in your cart. Perhaps you would be interested in one of the items below.', 'edd-empty-cart' ) ) . '</div>';
+	} else {
 		echo '<div class="edd-empty-cart-content">' . wpautop( $empty_cart_content ) . '</div>';
 	}
 
-	if ( !empty( $empty_cart_downloads ) ) {
-
-		$downloads = '<div class="edd-empty-cart-downloads">';
-		$downloads .= do_shortcode( $empty_cart_downloads );
-		$downloads .= '</div>';
-
-		echo $downloads;
+	if ( !isset( $empty_cart_downloads ) ) {
+		echo '<div class="edd-empty-cart-downloads">[downloads]</div>';
+	} else {
+		echo '<div class="edd-empty-cart-downloads">' . do_shortcode( $empty_cart_downloads ) . '</div>';
 	}
 
 	$content = ob_get_clean();
